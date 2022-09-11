3c109f7fa4a9b684e197ce3b3cce59643f49d5de
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-09-11 16:44:24 +0100

    Initial commit

---
+++ app/src/main/java/com/olisemeka/remybikes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.olisemeka.remybikes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_RemyBikes)
+        setContentView(R.layout.activity_main)
+    }
+}
