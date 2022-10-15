79c8e35fe11ba70416f0a41fe6bad5c6e71b6ec6
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-10-15 18:29:02 +0100

    Organized class files into appropriate packages

--- app/src/main/java/com/olisemeka/remybikes/ui/MainActivity.kt
+++ app/src/main/java/com/olisemeka/remybikes/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.olisemeka.remybikes
+package com.olisemeka.remybikes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.olisemeka.remybikes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/olisemeka/remybikes/ui/productsdisplay/ProductsDisplayFragment.kt
+++ app/src/main/java/com/olisemeka/remybikes/ui/productsdisplay/ProductsDisplayFragment.kt
@@ -1,4 +1,4 @@
-package com.olisemeka.remybikes
+package com.olisemeka.remybikes.ui.productsdisplay
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
