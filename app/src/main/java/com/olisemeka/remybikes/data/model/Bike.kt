70c3764d20d7b3101776a9b1f99aa12f1dc084d4
Nwaeme Olisemeka <35772751+nwaeme-olise@users.noreply.github.com>
2022-10-15 18:55:54 +0100

    Added Bike data class

---
+++ app/src/main/java/com/olisemeka/remybikes/data/model/Bike.kt
@@ -0,0 +1,14 @@
+package com.olisemeka.remybikes.data.model
+
+import android.text.SpannableString
+
+data class Bike(
+    private val name: String,
+    private val price: String,
+    private val brand: String,
+    private val imageUrl: String,
+    private var isInStock: Boolean,
+    private var rating: Float,
+    private var noOfReviews: Int,
+    private var productDetails: SpannableString
+)
